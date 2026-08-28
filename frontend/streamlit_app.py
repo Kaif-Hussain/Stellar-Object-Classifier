71d53ecc0cc09e6fdd3944f5840804db4f096e5e
Kaif-Hussain <hussainkaifhussain@gmail.com>
2026-08-28 20:38:10 +0530

    Update API_URL in Streamlit app and add new compiled Python file for backend

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import requests
 
-# Point this to your deployed backend URL once hosted, or localhost for local dev
-API_URL = "https://stellar-object-classifier-ofph.onrender.com/predict"
+API_URL = "https://stellar-object-classifier-ofph.onrender.com/predict"  
 
 st.set_page_config(page_title="Stellar Object Classifier", page_icon="🌌", layout="centered")
 
